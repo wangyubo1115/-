34284c1d029376f23ddcb845a544ff88ce36109e
wangyubo1115 <1147998268@qq.com>
2020-12-06 19:58:57 +0800

    for语句 练习

---
+++ 100~200的素数.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	int i,j;
+	for(i=100; i<=200;i++)
+	{
+		for (j=2;j<i;j++)
+		{
+			 if (i%j==0)
+			 {
+			    break;
+			 }
+		}
+		if (j==i)
+		{
+			printf("%d ", i);
+		}
+    }
+}
+
